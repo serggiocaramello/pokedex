5f16f5033121953c995b7e4ff1a128c3780524d7
marcelo33gonzalez <m.gonzalezm21@gmail.com>
2021-01-22 20:29:21 -0300

    entrega

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,18 @@ $(document).ready(function () {
   var inputMoves = $("#inputMoves");
   var inputPokemon = $("#inputPokemon");
   var selectPokemon = $("#selectPokemon");
+  var moves = "";
+  var movesName = $("#movesName");
+  var movesType = $("#movesType");
+  var movesAccuracy = $("#movesAccuracy");
+  var movesPowerPoints = $("#movesPowerPoints");
+  var movesPriority = $("#movesPriority");
+  var movesPower = $("#movesPower");
+  var movesDamageClass = $("#movesDamageClass");
+  var movesEffect = $("#movesEffect");
+  var movesGeneration = $("#movesGeneration");
+  var selectMove = $("#selectMove");
+  var movetitle = $("#movetitle");
 
   var labels = [
     "Hp",
@@ -144,6 +156,8 @@ $(document).ready(function () {
     obtenerPokemon();
   });
 
+  //tomas
+
   var llamarItem = function () {
     $.ajax({
       type: "GET",
@@ -175,7 +189,7 @@ $(document).ready(function () {
     dataType: "json",
     url: `https://pokeapi.co/api/v2/item/?limit=954`,
     success: function (data) {
-      console.log(data);
+      // console.log(data);
       for (var i = 0; i < data.count; i++) {
         selectItems.append(
           $(
@@ -206,4 +220,84 @@ $(document).ready(function () {
       itemEffect.text(data.effect_entries[0].effect);
     },
   });
+
+  //marcelo
+
+  for (var i = 1; i <= 784; i++) {
+    $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: `https://pokeapi.co/api/v2/move/${i}`,
+      success: function (data) {
+        // console.log(data);
+        selectMove.append(
+          $(`<option value="${data.name}">${data.name}</option>`)
+        );
+      },
+    });
+  }
+  //captura la seleccion del option en select
+  $("select#selectMove").on("change", function () {
+    var valor = $(this).val();
+    // alert(valor);
+
+    $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: `https://pokeapi.co/api/v2/move/${valor}`,
+      success: function (data) {
+        // console.log(data);
+        movetitle.text(data.name);
+        movesName.text(data.name);
+        movesType.text(data.type.name);
+        movesAccuracy.text(data.accuracy);
+        if (data.accuracy == null) {
+          movesAccuracy.text("-");
+        }
+        movesPowerPoints.text(data.pp);
+        movesPriority.text(data.priority);
+        movesPower.text(data.power);
+        if (data.power == null) {
+          movesPower.text("-");
+        }
+        movesDamageClass.text(data.damage_class.name);
+        movesEffect.text(data.effect_entries[0].effect);
+        movesGeneration.text(data.generation.name);
+      },
+    });
+  });
+
+  inputMoves.on("keypress", function (e) {
+    var code = e.keyCode ? e.keyCode : e.which;
+    if (code == 13) {
+      moves = this.value;
+
+      $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: `https://pokeapi.co/api/v2/move/${moves}`,
+        success: function (data) {
+          // console.log(data);
+          movetitle.text(data.name);
+          movesName.text(data.name);
+          movesType.text(data.type.name);
+          movesAccuracy.text(data.accuracy);
+          if (data.accuracy == null) {
+            movesAccuracy.text("-");
+          }
+          movesPowerPoints.text(data.pp);
+          movesPriority.text(data.priority);
+          movesPower.text(data.power);
+          if (data.power == null) {
+            movesPower.text("-");
+          }
+          movesDamageClass.text(data.damage_class.name);
+          movesEffect.text(data.effect_entries[0].effect);
+          movesGeneration.text(data.generation.name);
+        },
+      });
+    }
+  });
 });
+
+//final
